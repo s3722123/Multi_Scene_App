16d7f0519449893d81d5f0ff527dbfc781c45bc9
s3722123 <s3722123@student.rmit.edu.au>
2019-08-12 13:36:52 +1000

    Initial Commit

---
+++ Multi_Scene_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multi_Scene_App
+//
+//  Created by Lachlan  Wilson on 12/8/19.
+//  Copyright © 2019 Lachlan  Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
